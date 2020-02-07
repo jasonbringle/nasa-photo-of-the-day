be074dd46e94437fba263beec815ec9d1ba2f6c2
Jason <Jason@MacBook-Pro-3.local>
2020-02-06 16:34:22 -0800

    Button and Description components converted from .css to styled-components

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import "./Buttons.css"
+import styled from 'styled-components'
 
+const ButtonCont = styled.div`
+display:flex;
+    justify-content: center;
+
+    align-items: center;
+    height:25px;
+    margin: 2rem;
+`
+const Button = styled.button`
+    width: 40%;
+    height: 40px;
+    box-shadow: 5px 10px 18px rgb(31, 31, 31);
+    background-color: lightblue;
+    border-radius: 9px;
+    text-decoration:none;
+    font-size: 1rem;
+    outline: none;
+`;
 
 const Buttons = (props)=>{
     
 return (
-    <div className="buttons">
-        <button onClick={() => {
+    <ButtonCont>
+        <Button onClick={() => {
          props.setDay(props.randomDay)
          props.setMonth(props.randomMonth)
          props.setYear(props.randomYear())
         }}
         className= "random-pic">
             Random Pic!!
-            </button>
-    </div>
+        </Button>
+    </ButtonCont>
     )
 }
 export default Buttons
--- src/Description/Description.js
+++ src/Description/Description.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import './Description.css'
+import styled from 'styled-components'
 
+const MainDesc = styled.div`
+    display:flex;
+    flex-direction: row;
+`
+const Date = styled.div`
+display:flex;
+    justify-content: center;
+    font-style:italic;
+    color:white;
+`
+const Title = styled.div`
+border-top: 1px solid black;
+    color:white;
+`
+
+const Explain = styled.div`
+text-align: start;
+    border-bottom: 1px solid black;
+    color:white;
+`
+const DateAndTitle =styled.div`
+    display:flex;
+    flex-direction:column;
+`
 const Description = (props)=>{
  
 return (
 
-    <div className="main-Desc">
-        <div className='date'>
-            <p>{props.picData.date}</p>
-        </div>
-        <div className='title'>
-            <h2>{props.picData.title}</h2>
-        </div>
-        <div className='explain'>
+    <MainDesc>
+        <DateAndTitle>
+            <Title>
+                <h2>{props.picData.title}</h2>
+            </Title>
+            <Date>
+                <p>{props.picData.date}</p>
+            </Date>
+        </DateAndTitle>
+        <Explain>
             <p>{props.picData.explanation}</p>
-        </div>
-        
-    </div>
+        </Explain>
+    </MainDesc>
 
     )
 }
